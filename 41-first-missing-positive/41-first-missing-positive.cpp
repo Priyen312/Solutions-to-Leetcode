9ca93bdd408195f05ebec9a4d50dc30f09986670
Priyen312 <77093278+Priyen312@users.noreply.github.com>
2022-05-26 15:22:11 +0530

    Time: 268 ms (33.13%), Space: 83 MB (41.42%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,46 +1,7 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-
         
-        //sorting the input array (O(nlog(n))), (O(1))
-//         int n = 1;
-//         sort(nums.begin(), nums.end());
-//         for(int i = 0; i < nums.size(); i++)
-            
-//         {
-//             if(nums[i] == n)
-//             {
-//                 n++;
-//             }
-//             if(nums[i] > n)
-//             {
-//                 return n;
-//             }
-            
-//         }
-        
-//         return n;
-        
-        //using maps(O(n)), (O(n))
-        
-//         int n = 1; 
-//         unordered_map<int, int> m;
-//         for(int i = 0; i < nums.size(); i++)
-//         {
-//             m[nums[i]]++;
-//         }
-        
-//         for(int i = 1; i < nums.size() + 1; i++)
-//         {
-//             if(m.find(n) == m.end())
-//                 return n;
-//             else
-//                 n++;
-//         }
-        
-//         return n;
-            
          for(int i = 0; i < nums.size(); i++)
          {
             if(nums[i] <= 0)
@@ -69,6 +30,52 @@ public:
         
         return nums.size() + 1;
 
+    }
+    
+    int usingmaps(vector<int> &nums)
+    {
+        //using maps(O(n)), (O(n))
+        
+        int n = 1; 
+        unordered_map<int, int> m;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            m[nums[i]]++;
+        }
+        
+        for(int i = 1; i < nums.size() + 1; i++)
+        {
+            if(m.find(n) == m.end())
+                return n;
+            else
+                n++;
+        }
+        
+        return n;
+    }
+    
+    int usingsorting(vector<int> &nums)
+    {
+        //sorting the input array (O(nlog(n))), (O(1))
+        int n = 1;
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size(); i++)
+            
+        {
+            if(nums[i] == n)
+            {
+                n++;
+            }
+            if(nums[i] > n)
+            {
+                return n;
+            }
+            
+        }
+        
+        return n;
+        
+        
     }
     
 };
