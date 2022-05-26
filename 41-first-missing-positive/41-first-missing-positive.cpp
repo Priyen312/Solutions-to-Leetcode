ce0606946152fdf369af7639e2de06f55ed344f5
Priyen312 <77093278+Priyen312@users.noreply.github.com>
2022-05-26 15:16:01 +0530

    Time: 310 ms (25.21%), Space: 82.9 MB (78.04%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,23 +1,9 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-//       if(nums.size() == 1 && nums[0] == 1)
-//           return (nums[0] + 1);
-//         if(nums.size() == 1)
-//             return 1;
-//         int n = nums.size();
-//         vector<int> A = nums;
-//    int i = 0;while( i < n){
-//             if(A[i] > 0 && A[i] <= n && A[A[i] - 1] != A[i])
-//                 swap(A[i], A[A[i] - 1]);
-//         else
-//             ++ i;}
-//         for(int i = 0; i < n; ++ i)
-//             if(A[i] != i + 1)
-//                 return i + 1;
+
         
-//         return n + 1;
-        
+        //sorting the input array (O(nlog(n))), (O(1))
 //         int n = 1;
 //         sort(nums.begin(), nums.end());
 //         for(int i = 0; i < nums.size(); i++)
@@ -36,24 +22,53 @@ public:
         
 //         return n;
         
-        //using maps
+        //using maps(O(n)), (O(n))
         
-        int n = 1; 
-        unordered_map<int, int> m;
+//         int n = 1; 
+//         unordered_map<int, int> m;
+//         for(int i = 0; i < nums.size(); i++)
+//         {
+//             m[nums[i]]++;
+//         }
+        
+//         for(int i = 1; i < nums.size() + 1; i++)
+//         {
+//             if(m.find(n) == m.end())
+//                 return n;
+//             else
+//                 n++;
+//         }
+        
+//         return n;
+            
+         for(int i = 0; i < nums.size(); i++)
+         {
+            if(nums[i] <= 0)
+                nums[i] =  nums.size() + 1;
+         }
+    
         for(int i = 0; i < nums.size(); i++)
         {
-            m[nums[i]]++;
+            int index = abs(nums[i]);
+            
+            if(index <=nums.size())
+            {
+                if(nums[index - 1] > 0)
+                {
+                    nums[index - 1] *= -1;
+                }   
+            }
+
         }
-        
-        for(int i = 1; i < nums.size() + 1; i++)
+
+        for(int i = 0; i < nums.size(); i++)
         {
-            if(m.find(n) == m.end())
-                return n;
-            else
-                n++;
+            if(nums[i] > 0)
+                return i + 1;
         }
         
-        return n;
+        return nums.size() + 1;
+
     }
     
 };
