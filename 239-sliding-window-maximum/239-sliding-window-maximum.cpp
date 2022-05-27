ba3570cbc9bccf34738e4aa2a2f383ead65d8acd
Priyen312 <77093278+Priyen312@users.noreply.github.com>
2022-05-27 22:54:38 +0530

    Time: 605 ms (18.18%), Space: 134.8 MB (37.22%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -11,16 +11,22 @@ public:
         
         while(r<nums.size())
         {
-            while(queue.size() and nums[queue.back()] < nums[r])
+            while(queue.size() and nums[queue.back()] < nums[r])// make sure that no smaller values exist in the queue, just pop all the smaller values before pushing the current value
+                
                 queue.pop_back();
+            
+            
             queue.push_back(r);
             
-            if(l > queue.front())
+            
+            // if leftmost value in the queue is out of bounds of the window meaning it is lesser than left index then remove it from the queue because the window has moved on and the left most value which is the maximum is not related to the current window
+            if(queue.front() < l)
             {
                 queue.pop_front();
             }
             
-            if((1+r )>=k)
+            // using this condition before entering the value because we want that the window is smaller than the array itself before we enter any element
+            if((1+r)>=k)
             {
                 output.push_back(nums[queue.front()]);
                 l++;
