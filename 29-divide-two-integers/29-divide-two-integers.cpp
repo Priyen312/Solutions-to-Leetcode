412ccde806002588c43bb697ad63cc7bcb39f956
Priyen312 <77093278+Priyen312@users.noreply.github.com>
2022-05-27 17:03:43 +0530

    Time: 7 ms (26.86%), Space: 6 MB (30.35%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.cpp
+++ 29-divide-two-integers/29-divide-two-integers.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     int divide(int dividend, int divisor) {
         
-          if (dividend == INT_MIN && divisor == -1) {
+          if(dividend == divisor)
+            return 1;
+        bool isPositive = (dividend<0 == divisor<0);    // if both are of same sign, answer is positive
+        unsigned int a = abs(dividend);
+        unsigned int b = abs(divisor);
+        unsigned int ans = 0;
+        while(a >= b){  // while dividend is greater than or equal to divisor
+            short q = 0;
+            while(a > (b<<(q+1)))
+                q++;
+            ans += (1<<q);  // add the power of 2 found to the answer
+            a = a - (b<<q);  // reduce the dividend by divisor * power of 2 found
+        }
+        if(ans == (1<<31) and isPositive)   // if ans cannot be stored in signed int
             return INT_MAX;
-        }
-        long dvd = labs(dividend), dvs = labs(divisor), ans = 0;
-        int sign = dividend > 0 ^ divisor > 0 ? -1 : 1;
-        while (dvd >= dvs) {
-            long temp = dvs, m = 1;
-            while (temp << 1 <= dvd) {
-                temp <<= 1;
-                m <<= 1;
-            }
-            dvd -= temp;
-            ans += m;
-        }
-        return sign * ans;
+        return isPositive ? ans : -ans;
     }
 };
