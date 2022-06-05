376d2368854a2c548530f096f43141e3370050d7
Priyen312 <77093278+Priyen312@users.noreply.github.com>
2022-06-05 16:12:03 +0530

    Time: 15 ms (29.90%), Space: 10.5 MB (60.99%) - LeetHub

---
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        //We could keep 2 pointer,one for the start and another for the end of the current subarray, and make optimal moves so as to keep the \text{sum}sum greater than ss as well as maintain the lowest size possible.
+   int l = 0;
+        int sum = 0;
+        int res = INT_MAX;
+        
+        for(int r = 0; r< nums.size(); r++)
+        {
+            sum += nums[r];
+            
+            while(sum>=target)
+            {
+                res = min(res, r+1-l);
+                sum -= nums[l];
+                l++;
+            }
+        }
+        
+        return ((res!= INT_MAX) ? res : 0);
+    }
+};
