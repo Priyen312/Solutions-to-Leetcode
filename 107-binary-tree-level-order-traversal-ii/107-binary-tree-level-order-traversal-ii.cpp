a094d974c9417f2fcbeaba8b8946d540c79dccdb
Priyen312 <77093278+Priyen312@users.noreply.github.com>
2022-06-06 08:50:34 +0530

    Time: 7 ms (52.39%), Space: 13.4 MB (9.71%) - LeetHub

--- 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.cpp
+++ 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.cpp
@@ -11,48 +11,73 @@
  */
 class Solution {
 public:
-    vector<vector<int>> levelOrderBottom(TreeNode* root) {
-     vector<vector<int>> ans;
-    queue <TreeNode*> q;
+//     vector<vector<int>> levelOrderBottom(TreeNode* root) {
+//      vector<vector<int>> ans;
+//     queue <TreeNode*> q;
         
-        if(root == nullptr )
-            return ans;
+//         if(root == nullptr )
+//             return ans;
         
-        q.push(root);
+//         q.push(root);
         
-        while(q.empty() == false)
-        {vector <int> v;
-            int size = q.size();
+//         while(q.empty() == false)
+//         {vector <int> v;
+//             int size = q.size();
           
-         while(size-->0)
-         {
-              TreeNode* top = q.front();v.push_back(top->val);
-            q.pop();
+//          while(size-->0)
+//          {
+//               TreeNode* top = q.front();v.push_back(top->val);
+//             q.pop();
             
              
-             if(top->left !=nullptr)
-             {
+//              if(top->left !=nullptr)
+//              {
                  
-                q.push(top->left);
-                }
-            if(top->right !=nullptr)
-            {
-                q.push(top->right);
-            }
-         }
+//                 q.push(top->left);
+//                 }
+//             if(top->right !=nullptr)
+//             {
+//                 q.push(top->right);
+//             }
+//          }
          
-         ans.push_back(v);
+//          ans.push_back(v);
         
              
          
             
             
-        }
-        vector<vector<int>>helper;
-        while(ans.size())
+//         }
+//         vector<vector<int>>helper;
+//         while(ans.size())
+//         {
+//             helper.push_back(ans.back());ans.pop_back();
+//         }
+//         return helper;
+//     }
+    void l(TreeNode* root, int level, vector<vector<int>> &in)
+    {
+        if(root==nullptr) return;
+        
+        if(level >= in.size())
         {
-            helper.push_back(ans.back());ans.pop_back();
+            in.push_back(vector<int> ());
         }
-        return helper;
+        in[level].push_back(root->val);
+        l(root->left,level+1, in);
+        
+        l(root->right,level+1, in);
+        
+        
     }
+    
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        
+        vector<vector<int>> in;
+        l(root, 0, in);
+        reverse(in.begin(), in.end());
+        return in;
+    }
+    
+    
 };
