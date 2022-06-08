3b9ec9b7a184911d029b493ccd43b4ecaf957d4c
Priyen312 <77093278+Priyen312@users.noreply.github.com>
2022-06-08 22:42:40 +0530

    Time: 14 ms (91.86%), Space: 16.5 MB (80.91%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     int jump(vector<int>& A) {
-        int current = 0;
+        //The main idea is based on greedy. Let's say the range of the current jump is [curBegin, curEnd], curFarthest is the farthest point that all points in [curBegin, curEnd] can reach. Once the current point reaches curEnd, then trigger another jump, and set the new curEnd with curFarthest, 
+        
+        int start_to_see = 0;
         int jumps = 0;
         int farthest = 0;
         if(A.size() == 0)
@@ -10,17 +12,22 @@ public:
             return 0;
         if(A[0] == 0)
             return -1;
+        // i always want to jump to the node that has the highest value within my range
         for(int i = 0; i < A.size() - 1; i++)
         {
             farthest = max(farthest, i + A[i]);
+                                               
 
-            if(i == current)
+            if(i == start_to_see)
             {
                 jumps++;
-                current = farthest;
+                start_to_see = farthest;
+                if(start_to_see == A.size() - 1)
+                    return jumps;
             }
         }
 
         return jumps;
     }
+              
 };
