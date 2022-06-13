dd5dd3cf7823b81f986e79bfa721db1414438165
Priyen312 <77093278+Priyen312@users.noreply.github.com>
2022-06-13 21:49:42 +0530

    Time: 7 ms (77.48%), Space: 7.2 MB (72.11%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,71 +1,84 @@
 class Solution {
 public:
     
+    void create_chess(int n, vector<string> &grid)
+    {
+        string row = "";
+        for(int i = 0; i < n; i++)
+        {
+            row = "";
+            for(int j = 0; j < n; j++)
+            {
+                row = row + '.';
+            }
+            
+            grid.push_back(row);
+        }
+        
+    }
+    bool isSafe(int r, int c, vector<string> &grid)
+    {
+        
+        int n = grid.size();
+        int left = c;
+        int right = c;
+        while(r >= 0)
+        {
+            if(grid[r][c] == 'Q' ||
+               (left >= 0 and grid[r][left] == 'Q') || 
+               (right < n and grid[r][right] == 'Q')
+              )
+                
+                return false;
+            
+            r--;
+            left--;
+            right++;
+        }
+        
+        
+        return true;
+    }
     
-bool is_safe(vector<string> &matrix, int row, int col) {
-	
-    int n = matrix.size();
-	int col_left = col;
-	int col_right = col;
-	
-	while(row >=0)
-	{
-		if((col_left >=0 && matrix[row][col_left] == 'Q') || matrix[row][col]
-			== 'Q'		|| (col_right < n && matrix[row][col_right] == 'Q'))
-		   return false;
-		
-		   row--;
-		   col_left--;
-		   col_right++;
-	}
+    bool print_solutions(int n, int r, vector<string> &grid, vector<vector<string>> &all_possibilities)
+    {
+        if(r == n)
+            all_possibilities.push_back(grid);
+        
+        
+        else{
+            
+            for(int i = 0; i < n; i++)
+            {
+                if(isSafe(r, i, grid))
+                {
+                    grid[r][i] = 'Q';
 
-	return true;
-}
-bool solveNqueens(vector<string> &matrix, int n, int row, vector<vector<string>> &result) {
-	
-	if(row == n)
-	{
-		result.push_back(matrix);
-	}
-	else
-	{
-		for(int i = 0; i< n; i++)
-		{
-			if(is_safe(matrix,row,i))
-			{
-				matrix[row][i] = 'Q';
-				bool b = solveNqueens(matrix,n,row + 1, result);
-				if (b)
-				{
-					return true;
-				}
-				
-				matrix[row][i] = '.'; //backtracking
-			}
-		}
-	}
-	
-	return false;
-	
-}
+                    bool check_for_current = print_solutions(n, r + 1, grid, all_possibilities);
+                    if(check_for_current)
+                    {
+                        return true;
+                    }
+
+                    grid[r][i] = '.';
+
+                }
+            }
+            }
+        
+        return false;
+    }
 
     vector<vector<string>> solveNQueens(int n) {
         
-        vector<vector<string>> result;
-        vector<string> matrix;
-        string row = "";
-        for (int i = 0; i < n; i++) 
-        {
-            row = "";
-            for (int j = 0; j < n; j++) 
-            {
-                row += '.';
-            }
-            matrix.push_back(row);
-        }
-        solveNqueens(matrix, n, 0, result);
-        return result;
         
+        vector<string> grid;
+        vector<vector<string>> chess_board;
+        
+        create_chess(n, grid);
+        
+        print_solutions(n, 0, grid, chess_board);
+        
+        return chess_board;
     }
-        
 };
