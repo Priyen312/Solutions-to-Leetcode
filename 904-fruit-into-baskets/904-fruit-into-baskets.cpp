ada2dc99d37b24699c1f90cd41c717ee9ffa9a7f
Priyen312 <77093278+Priyen312@users.noreply.github.com>
2022-06-24 12:45:00 +0530

    Time: 148 ms (90.21%), Space: 66.1 MB (98.63%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.cpp
+++ 904-fruit-into-baskets/904-fruit-into-baskets.cpp
@@ -3,50 +3,40 @@ public:
     int totalFruit(vector<int>& fruits) {
         
        
-        int fruit1 = 0, fruit2 = 0;
-        int type1 = -1, type2 = -1;
+        int last_fruit = -1, second_last_fruit = -1;
+        int last_fruit_count = 0;
         
-        int current_max = 0;
+        int curr_maximum = 0;
         int maximum = 0;
         
-        
-       
-        
-        
-        
-        
-        for(int r = 0; r < fruits.size(); r++)
+        for(auto curr_fruit : fruits)
         {
-            
-            int current_type = fruits[r];
-            if(current_type == type1 || current_type == type2)//check if the current fruit is in the bastket
+            if(curr_fruit == last_fruit | curr_fruit == second_last_fruit)
             {
-                current_max++;
-            }
-            else// if not, then the fruit just before the current fruit is kept and the current fruit is also kept
-            {
-                current_max = fruit2 + 1;
+                curr_maximum++;
             }
             
-            if(current_type == type2)// if the current fruit is the last fruit in the bag then increase its count
+            else
             {
-                fruit2++;
-            }
-            else// else it is already in bag or a new fruit, either way make the count of last seen fruit as 1
-            {
-                fruit2 = 1;
+                curr_maximum = last_fruit_count + 1;
+                                
             }
             
-            if(current_type != type2)// if the new fruit that was seen wasn't the last seen fruit then it either is a new fruit or a fruit from the other basket, either way we change the values
+            if(curr_fruit == last_fruit)
             {
-                type1 = type2;
-                type2 = current_type;
-                
-                
+                last_fruit_count++;
+            }
+            else
+            {
+                last_fruit_count = 1;
             }
                
-            
-            maximum = max(maximum, current_max);
+            if(curr_fruit != last_fruit)
+            {
+                second_last_fruit = last_fruit;
+                last_fruit = curr_fruit;
+            }
+            maximum = max(maximum, curr_maximum);
             
         }
         return maximum;
