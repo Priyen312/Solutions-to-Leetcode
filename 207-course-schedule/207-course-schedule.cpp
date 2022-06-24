2675fcbfde991a6f5e171d1a26b6e1bc243610d7
Priyen312 <77093278+Priyen312@users.noreply.github.com>
2022-06-24 21:33:01 +0530

    Time: 46 ms (21.17%), Space: 14.7 MB (25.93%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,53 +1,89 @@
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses, vector<int>());
-        vector<int> indegree(numCourses, 0);
+    
+    bool cycle(int &course, vector<vector<int>> &adj, vector<int> &vis)
+    {
+        if(vis[course] == 1)
+            return true;
+        if(vis[course] == 0)
+        {    
+            vis[course] = 1;
+            for(auto edge: adj[course])
+            {
+
+                if(cycle(edge, adj, vis))
+                    return true;
+            }
+        }
         
+        vis[course] = 2;
+        return false;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites)
+    {
+        vector<vector<int>> adj(numCourses);
         for(auto i: prerequisites)
         {
-            indegree[i[0]]++;// [a,b] b->a a++
             adj[i[1]].push_back(i[0]);
         }
+        vector<int> vis(numCourses, 0);
         
-        vector<int> visited(numCourses, 0);
-        
-        queue<int> q;
-        int count = 0;
-        for(int i = 0; i < indegree.size(); i++)
+        for(int course = 0; course < numCourses; course++)
         {
-            if(indegree[i] == 0)
-            {
-                q.push(i);// starting node
-                visited[i] = 1;
-                count++;
-            }
+            if(cycle(course, adj, vis))
+                return false;
         }
         
-        while(q.size())
-        {
-            int t = q.front();
-            q.pop();
-            
-            for(auto n: adj[t])
-            {
-                if(visited[n] == 0)
-                {
-                    indegree[n]--;
-                    if(indegree[n] == 0)// all the prerequisites for the course is met 
-                    {
-                        q.push(n);
-                        visited[n] = 1;
-                        count++;
-                    }
-                    
-                }
-                
-                
-            }
-        }
-        
-        return (count == numCourses);
-             
+        return true;
     }
+//     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+//         vector<vector<int>> adj(numCourses, vector<int>());
+//         vector<int> indegree(numCourses, 0);
+        
+//         for(auto i: prerequisites)
+//         {
+//             indegree[i[0]]++;// [a,b] b->a a++
+//             adj[i[1]].push_back(i[0]);
+//         }
+        
+//         vector<int> visited(numCourses, 0);
+        
+//         queue<int> q;
+//         int count = 0;
+//         for(int i = 0; i < indegree.size(); i++)
+//         {
+//             if(indegree[i] == 0)
+//             {
+//                 q.push(i);// starting node
+//                 visited[i] = 1;
+//                 count++;
+//             }
+//         }
+        
+//         while(q.size())
+//         {
+//             int t = q.front();
+//             q.pop();
+            
+//             for(auto n: adj[t])
+//             {
+//                 if(visited[n] == 0)
+//                 {
+//                     indegree[n]--;
+//                     if(indegree[n] == 0)// all the prerequisites for the course is met 
+//                     {
+//                         q.push(n);
+//                         visited[n] = 1;
+//                         count++;
+//                     }
+                    
+//                 }
+                
+                
+//             }
+//         }
+        
+//         return (count == numCourses);
+             
+//     }
 };
