70de1482999210e57071d6ee6d08a82b801f8a6e
Priyen312 <77093278+Priyen312@users.noreply.github.com>
2022-06-24 19:50:37 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (88.78%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.cpp
+++ 767-reorganize-string/767-reorganize-string.cpp
@@ -1,73 +1,56 @@
 class Solution {
 public:
     
-struct Key {
-    int freq; 
-    char ch;
- 
-   
-    bool operator<(const Key& k) const
-    {
-        return freq < k.freq;
-    }
-};
- 
 
     string reorganizeString(string s) {
         
-    int n = s.length();
- 
-    
-    int count[26] = { 0 };
-    for (int i = 0; i < n; i++)
-        count[s[i] - 'a']++;
- 
-    
-    priority_queue<Key> pq;
-    for (char c = 'a'; c <= 'z'; c++) {
-          int val = c - 'a';
-        if (count[val]) {
-            pq.push(Key{ count[val], c });
+        vector<int> freq(26);
+        //calculating the frequency of each character
+        int max_freq = 0;
+        
+        for(auto c: s)
+        {
+            freq[c - 'a']++;
+            if(freq[c - 'a'] > freq[max_freq])
+            {
+                max_freq = c - 'a';
+                
+            }
         }
+        
+        
+        if(freq[max_freq]*2 - 1 > s.size())// if even a single character has a frequency greater than half the length of the string then the string cannot be organised because the length would now exceed og length after reorganisation
+            return "";
+        int index = 0;
+        // position the maximum freq char in the even positions first all the while reducing it's freq to zero
+        while(freq[max_freq])
+        {
+            s[index] = 'a' + max_freq;
+            index += 2;
+            freq[max_freq]--;
+        }
+        
+        for(int i = 0; i < 26; i++)
+        {
+            
+            while(freq[i])
+            {
+                if(index >= s.size())
+                {
+                    index = 1;
+                }
+                
+                s[index] = 'a' + i;
+                index += 2;
+                freq[i]--;
+            }
+        }
+        
+        
+        
+        return s;
+        
     }
- 
-    
-    string str = "";
- 
-    // work as the previous visited element
-    // initial previous element be. ( '#' and
-    // it's frequency '-1' )
-    Key prev{ -1, '#' };
- 
-    // traverse queue
-    while (!pq.empty()) {
-        
-        Key k = pq.top();
-        pq.pop();
-        str = str + k.ch;
- 
-        // IF frequency of previous character is less
-        // than zero that means it is useless, we
-        // need not to push it
-        if (prev.freq > 0)
-            pq.push(prev);
- 
-        // decrease frequency by 'one'
-        
-        
-        (k.freq)--;
-        
-        // make current character as the previous 'char'
-        prev = k;
-    }
- 
-    // If length of the resultant string and original
-    // string is not same then string is not valid
-    if (n != str.length())
-        return "";
- 
-    else return str;
-}
         
     
 };
