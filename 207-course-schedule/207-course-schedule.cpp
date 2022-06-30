8357ae235941fe197cd22f6e306d41136290bc8f
Priyen312 <77093278+Priyen312@users.noreply.github.com>
2022-06-30 22:10:51 +0530

    Time: 38 ms (37.75%), Space: 14.9 MB (20.52%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
-    
+    vector<int> order;
     bool cycle(int &course, vector<vector<int>> &adj, vector<int> &vis)
-    {
+    {//We just have to find if our graph contains cycle or not because if graph contains cycle then all tnodes in cycle are interdependent and 1 course cannot be completed because its prerequisite is dependent on other course and it goes on .
+// We used coloring algorithm to find if there is cycle in graph or not.
+
+// Coloring Algorithm
+// vis[id]=0 is used for node which is not yet visited, vis[id]=1 is used for the node which is visited and currently its child nodes are being visited and vis[id]=2 done when all the child nodes of a node ("id") are visited and the function returns to parent node of node ("id"). So at that time it is marked as 2 because this node does not require any further traversing.
         if(vis[course] == 1)
             return true;
         if(vis[course] == 0)
@@ -17,6 +21,7 @@ public:
         }
         
         vis[course] = 2;
+        order.push_back(course);
         return false;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites)
@@ -27,14 +32,31 @@ public:
             adj[i[1]].push_back(i[0]);
         }
         vector<int> vis(numCourses, 0);
-        
+        if(order.size() == numCourses)
+        {
+            for(auto course: order)
+                cout<< course<< " ";
+            
+            cout<<endl;
+        }
+        else cout<< "";
         for(int course = 0; course < numCourses; course++)
         {
-            if(cycle(course, adj, vis))
+            if(vis[course] != 2 and cycle(course, adj, vis))
                 return false;
         }
         
+        if(order.size() == numCourses)
+        {
+            for(auto course: order)
+                cout<< course<< " ";
+            
+            cout<<endl;
+        }
+        else cout<< "";
+        
         return true;
+        
     }
 //     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
 //         vector<vector<int>> adj(numCourses, vector<int>());
