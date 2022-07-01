35e3c7dbdda92a7e234319493818d7436308beaa
Priyen312 <77093278+Priyen312@users.noreply.github.com>
2022-07-01 12:27:22 +0530

    Time: 18 ms (44.07%), Space: 9.4 MB (50.56%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,39 +1,64 @@
 class Solution {
 public:
-    
-    void generate_permutations(vector<int>& nums, vector<vector<int>> &permutations, int index)
-    {
-        if(index == nums.size())
-        {
-            permutations.push_back(nums);
+    void recursion(vector<int> num, int index, int size, vector<vector<int> > &res) {
+        if (index == size) {
+            res.push_back(num);
             return;
         }
         
-       unordered_set<int> s;
-        
-        for(int i = index; i < nums.size(); i++)
-        {
-            if(s.find(nums[i]) == s.end())// check if the number in the array has already been considered for the permutation
-            {
-                s.insert(nums[i]);
-                
-                swap(nums[index], nums[i]);
-                generate_permutations(nums, permutations, index + 1);
-                swap(nums[index], nums[i]);
-                
-                
-            }
+        for (int k = index; k < size; k++) {
             
-                       
+            if (index != k && num[index] == num[k]) 
+                continue;
+            swap(num[index], num[k]);
+            
+            recursion(num, index + 1, size, res);
         }
     }
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
+    
+    vector<vector<int> > permuteUnique(vector<int> &num) {
         
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> permutations;
+        sort(num.begin(), num.end());
         
-        generate_permutations(nums, permutations, 0);
+        vector<vector<int> >res;
         
-        return permutations;
+        recursion(num, 0, num.size(), res);
+        
+        return res;
     }
+//     void generate_permutations(vector<int>& nums, vector<vector<int>> &permutations, int index)
+//     {//This is like the Permutation I solution, but we use an unordered_set to avoid swapping elements that are the same (which would generate identical permutations).
+//         if(index == nums.size())
+//         {
+//             permutations.push_back(nums);
+//             return;
+//         }
+        
+//        unordered_set<int> s;
+        
+//         for(int i = index; i < nums.size(); i++)
+//         {
+//             if(s.find(nums[i]) == s.end())// check if the number in the array has already been considered for the permutation
+//             {
+//                 s.insert(nums[i]);
+                
+//                 swap(nums[index], nums[i]);
+//                 generate_permutations(nums, permutations, index + 1);
+//                 swap(nums[index], nums[i]);
+                
+                
+//             }
+            
+                       
+//         }
+//     }
+//     vector<vector<int>> permuteUnique(vector<int>& nums) {
+        
+//         sort(nums.begin(), nums.end());
+//         vector<vector<int>> permutations;
+        
+//         generate_permutations(nums, permutations, 0);
+        
+//         return permutations;
+//     }
 };
