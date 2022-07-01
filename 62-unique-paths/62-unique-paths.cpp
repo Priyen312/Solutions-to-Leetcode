d486634a1bd90824a58797737de06d1e09586f65
Priyen312 <77093278+Priyen312@users.noreply.github.com>
2022-07-01 22:04:59 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (25.42%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int move(int m, int n, vector<vector<int>> &dp)
-{
+{//O(n*m), O(n*m)
 	if(m == 1 or n == 1)
 		return 1;
-        
+    if(m < 1 or n < 1)   
+        return 0;
 	if(dp[m][n] != -1)
 		return dp[m][n];
 	
-	return dp[m][n] = move(m-1, n, dp) + move(m, n - 1, dp);
+	return dp[m][n] = move(m - 1, n, dp) + move(m, n - 1, dp);
 }
 
 int uniquePaths(int m, int n) {
