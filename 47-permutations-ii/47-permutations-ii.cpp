ec7f52c71d94594b9bc3144e4439a46458dab1d6
Priyen312 <77093278+Priyen312@users.noreply.github.com>
2022-07-01 12:34:48 +0530

    Time: 36 ms (32.10%), Space: 15.1 MB (14.84%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,31 +1,76 @@
 class Solution {
 public:
-    void recursion(vector<int> num, int index, int size, vector<vector<int> > &res) {
-        if (index == size) {
-            res.push_back(num);
+     vector<vector<int>> permuteUnique(vector <int> nums) {
+        vector<vector<int>> results;
+
+        
+        unordered_map<int, int> counter;
+        
+         for (int num : nums) {
+            counter[num]++;
+        }
+
+        vector<int> comb;
+        backtrack(comb, nums.size(), counter, results);
+        return results;
+    }
+
+    void backtrack(vector<int> comb, int N, unordered_map<int, int> counter, vector<vector<int>> &results) {
+
+        if (comb.size() == N) {
+            results.push_back(comb);
             return;
         }
-        
-        for (int k = index; k < size; k++) {
+
+        for(auto i: counter)
+        {
+            int num = i.first;
+            int count = i.second;
             
-            if (index != k && num[index] == num[k]) 
+            if(count == 0)
                 continue;
-            swap(num[index], num[k]);
             
-            recursion(num, index + 1, size, res);
+            comb.push_back(num);
+            counter[num] = count - 1;
+            
+            backtrack(comb, N, counter, results);
+            
+            comb.pop_back();
+            counter[num] = count;
+                
         }
+        
+            
     }
+//     void recursion(vector<int> num, int index, int size, vector<vector<int> > &res) {
+//         if (index == size) {
+//             res.push_back(num);
+//             return;
+//         }
+        
+//         for (int k = index; k < size; k++) {
+            
+//             if (index != k && num[index] == num[k]) 
+//                 continue;
+//             swap(num[index], num[k]);
+            
+//             recursion(num, index + 1, size, res);
+//         }
+//     }
     
-    vector<vector<int> > permuteUnique(vector<int> &num) {
+//     vector<vector<int> > permuteUnique(vector<int> &num) {
         
-        sort(num.begin(), num.end());
+//         sort(num.begin(), num.end());
         
-        vector<vector<int> >res;
+//         vector<vector<int> >res;
         
-        recursion(num, 0, num.size(), res);
+//         recursion(num, 0, num.size(), res);
         
-        return res;
-    }
+//         return res;
+//     }
+    
+    
+    
 //     void generate_permutations(vector<int>& nums, vector<vector<int>> &permutations, int index)
 //     {//This is like the Permutation I solution, but we use an unordered_set to avoid swapping elements that are the same (which would generate identical permutations).
 //         if(index == nums.size())
