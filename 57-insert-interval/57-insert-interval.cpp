3a172559f6226b385d05899033fde586207b65db
Priyen312 <77093278+Priyen312@users.noreply.github.com>
2022-07-03 21:24:30 +0530

    Time: 30 ms (35.47%), Space: 17.2 MB (37.85%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -8,18 +8,18 @@ public:
             int start = newInterval[0];
             int end = newInterval[1];
             
-            if(end < interval[i][0])
+            if(end < interval[i][0])//not overlapping and finish before other meeting starts thus inserted and a positon for current meeting is being looked for
             {
                 combined.push_back(newInterval);
                 newInterval = interval[i];
             }
             
-            else if(start > interval[i][1])
+            else if(start > interval[i][1])//not overlapping and starts after the current meeting ends this current meeting is added
             {
                 combined.push_back(interval[i]);
             }
             
-            else
+            else// overlap, don't add it yet because the next meeting could potentially overlap
             {
                 newInterval = {min(start, interval[i][0]), max(end, interval[i][1])};
             }
