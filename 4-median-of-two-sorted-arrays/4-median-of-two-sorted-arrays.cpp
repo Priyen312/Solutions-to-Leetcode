a854f34f5acf224815e5b263cbec06e94d6e19e5
Priyen312 <77093278+Priyen312@users.noreply.github.com>
2022-07-04 11:33:58 +0530

    Time: 55 ms (61.89%), Space: 89.3 MB (61.10%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -2,7 +2,10 @@ class Solution {
 public:
 
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        // https://www.youtube.com/watch?v=q6IEA26hvXc
         
+        //log(min(n1, n2)) bcoz we are running binary search on the smaller of the two arrays
+        //any number in the two left halves <= any number in the two right halves
         int n1 = nums1.size(), n2 = nums2.size();
         if(n1 > n2)
             return findMedianSortedArrays(nums2, nums1);
@@ -15,7 +18,7 @@ public:
             
             int mid1 = (l + h) / 2;// exact middle of first array
             int mid2 = (n1 + n2 + 1) / 2  // middle of merged array
-                        - mid1; // minus the middle that has already been taken
+                                - mid1; // minus the middle that has already been taken
             
                 
             int L1 = mid1 == 0 ? INT_MIN : nums1[mid1 - 1];
@@ -41,4 +44,5 @@ public:
         
         return 0.0;
     }
+
 };
