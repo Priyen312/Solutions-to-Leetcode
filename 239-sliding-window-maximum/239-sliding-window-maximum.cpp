b09a2f795b252f26fc7b95eac0c65f7403fe1d32
Priyen312 <77093278+Priyen312@users.noreply.github.com>
2022-07-05 21:32:05 +0530

    Time: 536 ms (32.75%), Space: 134.7 MB (71.30%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -5,34 +5,37 @@ public:
         
         vector<int> output;
         
-        int l=0;
-        int r =0; // the window
-        //the queue will have the valeus in decreasing order thus the front will always be greater one in window
-        
-        while(r<nums.size())
+        int r = 0; // the window
+        //the queue will have the values in decreasing order thus the front will always be greater one in window
+        for(r = 0; r < k; r++)
         {
-            while(queue.size() and nums[queue.back()] < nums[r])// make sure that no smaller values exist in the queue, just pop all the smaller values before pushing the current value
+            while(queue.size() and nums[queue.back()] <= nums[r])// make sure that no smaller values exist in the queue, just pop all the smaller values before pushing the current value
+                
+                queue.pop_back();
+            
+            
+            queue.push_back(r);
+        }
+        while(r < nums.size())
+        {
+            output.push_back(nums[queue.front()]);
+            
+            
+            // if leftmost value in the queue is out of bounds of the window meaning it is lesser than left index then remove it from the queue because the window has moved on and the left most value which is the maximum is not related to the current window
+            while(queue.size() and queue.front() <= r - k)
+                queue.pop_front();
+            
+            
+            while(queue.size() and nums[queue.back()] <= nums[r])// make sure that no smaller values exist in the queue, just pop all the smaller values before pushing the current value
                 
                 queue.pop_back();
             
             
             queue.push_back(r);
             
-            
-            // if leftmost value in the queue is out of bounds of the window meaning it is lesser than left index then remove it from the queue because the window has moved on and the left most value which is the maximum is not related to the current window
-            if(queue.front() < l)
-            {
-                queue.pop_front();
-            }
-            
-            // using this condition before entering the value because we want that the window is smaller than the array itself before we enter any element
-            if((1+r)>=k)
-            {
-                output.push_back(nums[queue.front()]);
-                l++;
-            }
-            
             r++;
-        }return output;
+        }
+        output.push_back(nums[queue.front()]);
+        return output;
     }
 };
