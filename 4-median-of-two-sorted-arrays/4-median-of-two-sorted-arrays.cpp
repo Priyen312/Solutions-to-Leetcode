9ae2e709fb327c1f3172c832a8b8de8ac3137306
Priyen312 <77093278+Priyen312@users.noreply.github.com>
2022-07-06 23:44:45 +0530

    Time: 39 ms (91.75%), Space: 89.4 MB (61.27%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,141 +1,143 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& a, vector<int>& b)
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
     {
         int i = 0, j = 0;
-    int size = a.size() + b.size();
-    int c = 0;
-    bool get_next = false;
-    if(size % 2)
-    {
-        c = size / 2;
-    }
-    else
-    {
-        c = (size / 2) - 1;
-        get_next = true;
-    }
-    double median = 0.0;
-    while(i < a.size() and j < b.size())
-    {
-        if(a[i] < b[j])
+        int size = nums1.size() + nums2.size();
+        int c = 0;
+        bool get_next = false;
+        if(size % 2)
         {
-            
+            c = size / 2;
+        }
+        else
+        {
+            c = (size / 2) - 1;
+            get_next = true;
+        }
+        double median = 0.0;
+        while(i < nums1.size() and j < nums2.size())
+        {
+            if(nums1[i] < nums2[j])
+            {
+
+                if(c == 0)
+                {
+                    median = nums1[i];
+                    if(get_next == false)
+                        return median;
+                }
+                else if((c == -1))
+                {
+                    return (median + nums1[i]) / 2.0;
+                }
+                c--;
+                i++;
+            }
+
+            else
+            {
+
+                if(c == 0)
+                {
+                    median = nums2[j];
+                    if(get_next == false)
+                        return median;
+                }
+                else if((c == -1))
+                {
+                    return (median + nums2[j]) / 2.0;
+                }
+
+                j++;
+                c--;
+            }
+        }
+
+
+        while(i < nums1.size())
+        {
+
             if(c == 0)
             {
-                median = a[i];
+                median = nums1[i];
                 if(get_next == false)
                     return median;
             }
             else if((c == -1))
             {
-                return (median + a[i]) / 2.0;
+                return (median + nums1[i]) / 2.0;
             }
             c--;
             i++;
         }
-        
-        else
+
+        while(j < nums2.size())
         {
-           
+
             if(c == 0)
             {
-                median = b[j];
+                median = nums2[j];
                 if(get_next == false)
                     return median;
             }
             else if((c == -1))
             {
-                return (median + b[j]) / 2.0;
+                return (median + nums2[j]) / 2.0;
             }
-            
-            j++;
             c--;
+            j++;
         }
+
+
+        return 0.0;
     }
     
-    
-    while(i < a.size())
-    {
+
+    double binary_searchMedian(vector<int>& nums1, vector<int>& nums2) {
+        // https://www.youtube.com/watch?v=q6IEA26hvXc
         
-        if(c == 0)
-        {
-            median = a[i];
-            if(get_next == false)
-                return median;
-        }
-        else if((c == -1))
-        {
-            return (median + a[i]) / 2.0;
-        }
-        c--;
-        i++;
-    }
-    
-    while(j < b.size())
-    {
-        
-        if(c == 0)
-        {
-            median = b[j];
-            if(get_next == false)
-                return median;
-        }
-        else if((c == -1))
-        {
-            return (median + b[j]) / 2.0;
-        }
-        c--;
-        j++;
-    }
-    
-    
-    return 0.0;
-    }
-//     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-//         // https://www.youtube.com/watch?v=q6IEA26hvXc
-        
-//         //log(min(n1, n2)) bcoz we are running binary search on the smaller of the two arrays
-//         //any number in the two left halves <= any number in the two right halves
-//         int n1 = nums1.size(), n2 = nums2.size();
-//         if(n1 > n2)
-//             return findMedianSortedArrays(nums2, nums1);
+        //log(min(n1, n2)) bcoz we are running binary search on the smaller of the two arrays
+        //any number in the two left halves <= any number in the two right halves
+        int n1 = nums1.size(), n2 = nums2.size();
+        if(n1 > n2)
+            return findMedianSortedArrays(nums2, nums1);
             
-//         int l = 0;// low of first array
-//         int h = n1;// low of second array
+        int l = 0;// low of first array
+        int h = n1;// low of second array
         
-//         while(l <= h)
-//         {
+        while(l <= h)
+        {
             
-//             int mid1 = (l + h) / 2;// exact middle of first array
-//             int mid2 = (n1 + n2) / 2  // middle of merged array
-//                                 - mid1; // minus the middle that has already been taken
+            int mid1 = (l + h) / 2;// exact middle of first array
+            int mid2 = (n1 + n2) / 2  // middle of merged array
+                                - mid1; // minus the middle that has already been taken
             
                 
-//             int L1 = mid1 == 0 ? INT_MIN : nums1[mid1 - 1];
-//             int L2 = mid2 == 0 ? INT_MIN : nums2[mid2 - 1];
+            int L1 = mid1 == 0 ? INT_MIN : nums1[mid1 - 1];
+            int L2 = mid2 == 0 ? INT_MIN : nums2[mid2 - 1];
             
-//             int H1 = mid1 == n1 ? INT_MAX : nums1[mid1];
-//             int H2 = mid2 == n2 ? INT_MAX : nums2[mid2];
+            int H1 = mid1 == n1 ? INT_MAX : nums1[mid1];
+            int H2 = mid2 == n2 ? INT_MAX : nums2[mid2];
                 
             
             
-//             if(L1 > H2)
-//                 h = mid1 - 1;
+            if(L1 > H2)
+                h = mid1 - 1;
             
-//             else if(L2 > H1)
-//                 l = mid1 + 1;
+            else if(L2 > H1)
+                l = mid1 + 1;
             
-//             else
-//             {
-//                 if((n1 + n2) % 2 == 1)
-//                     return min(H1, H2);
+            else
+            {
+                if((n1 + n2) % 2 == 1)
+                    return min(H1, H2);
                 
-//                     return (max(L1, L2) + min(H1, H2)) / 2.0;
-//             }
-//         }
+                    return (max(L1, L2) + min(H1, H2)) / 2.0;
+            }
+        }
         
-//         return 0.0;
-//     }
+        return 0.0;
+    }
 
 };
