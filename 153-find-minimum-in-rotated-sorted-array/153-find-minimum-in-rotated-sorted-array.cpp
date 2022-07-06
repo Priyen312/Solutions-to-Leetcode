0f8446293450a66cea20d07736ca170cd111048a
Priyen312 <77093278+Priyen312@users.noreply.github.com>
2022-07-06 19:49:17 +0530

    Time: 15 ms (6.17%), Space: 10.1 MB (71.72%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,39 +1,63 @@
 class Solution {
 public:
-    int findMin(vector<int>& nums) {
-        int n = nums.size();
-        if(n == 1)
-            return nums[0];
+    //Find the mid element of the array.
+
+// If mid element > first element of array this means that we need to look for the inflection point on the right of mid.
+
+// If mid element < first element of array this that we need to look for the inflection point on the left of mid.
+//     int findMin(vector<int>& nums) {
+//         int n = nums.size();
+//         if(n == 1)
+//             return nums[0];
         
-        int l = 0, r = n - 1;
+//         int l = 0, r = n - 1;
         
-        if(nums[r] > nums[0])
-            return nums[0];
+//         if(nums[r] > nums[0])
+//             return nums[0];
         
-        while(r >= l)
+//         while(r >= l)
+//         {
+//             int mid = l + (r - l) / 2;
+            
+//             if(nums[mid] > nums[mid + 1])
+//             {
+//                 return nums[mid + 1];
+//             }
+            
+//             if(nums[mid] < nums[mid - 1])
+//             {
+//                 return nums[mid];
+//             }
+            
+            
+//             if(nums[mid] > nums[0])
+//                 l = mid + 1;
+//             else
+//             {
+//                 r = mid - 1;
+//             }
+//         }
+        
+        
+//         return INT_MAX;
+//     }
+    
+    int findMin(vector<int>& nums)
+    {
+        int l = 0, r = nums.size() - 1;
+         
+        while(l < r) // find the minimum
         {
-            int mid = l + (r - l) / 2;
+            int mid = (l + r)/2;
             
-            if(nums[mid] > nums[mid + 1])
+            if(nums[mid] > nums[r])
             {
-                return nums[mid + 1];
-            }
-            
-            if(nums[mid] < nums[mid - 1])
-            {
-                return nums[mid];
-            }
-            
-            
-            if(nums[mid] > nums[0])
                 l = mid + 1;
-            else
-            {
-                r = mid - 1;
             }
+            else
+                r = mid;
         }
         
-        
-        return INT_MAX;
+        return nums[l];
     }
 };
