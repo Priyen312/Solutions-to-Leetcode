33892b8b9aaa6581ccd13fb873dbe90eb3f115bf
Priyen312 <77093278+Priyen312@users.noreply.github.com>
2022-07-13 22:38:50 +0530

    Time: 73 ms (23.48%), Space: 12.3 MB (83.64%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
 void dfs(vector<vector<char>> &board, int i, int j, map<pair<int,int>,int> &visited)
-    {
-        if(i<0 or j<0 or i>=board.size() or j>=board[0].size() or  board[i][j] != '1' or visited[{i,j}] == 1)
+    {//O(n^2) as each grid position gets visited once
+        if(i < 0 or j < 0 or i >= board.size() or j >= board[0].size() or  board[i][j] == '0' or visited[{i,j}] == 1)
             return;
 
         visited[{i,j}] = 1;
         
-    
         dfs(board, i + 1, j, visited);
         dfs(board, i , j + 1, visited);
         dfs(board, i - 1, j, visited);
         dfs(board, i , j - 1, visited);
-
-        
-        
+  
     }
   
+void mark3(vector<vector<char>> &board, int i, int j)
+{
+    if(i < 0 or j < 0 or i >= board.size() or j >= board[0].size() or  board[i][j] != '1')
+            return;
 
+        
+        board[i][j] = 3;
+    
+        mark3(board, i + 1, j);
+        mark3(board, i , j + 1);
+        mark3(board, i - 1, j);
+        mark3(board, i , j - 1);
+    
+}
 int numIslands(vector<vector<char>> &surface) {
     // add your logic here
 	map<pair<int,int>, int> visited;
@@ -26,9 +36,9 @@ int numIslands(vector<vector<char>> &surface) {
     {
         for(int j = 0; j < surface[0].size(); j++)
         {
-            if(surface[i][j] == '1' && visited.find({i,j}) == visited.end())
+            if(surface[i][j] == '1')
             {
-                dfs(surface, i, j, visited);
+                mark3(surface, i, j);
                 islands++;
             }
         }
