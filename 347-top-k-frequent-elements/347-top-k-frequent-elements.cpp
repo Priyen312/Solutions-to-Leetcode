910cf4b6b633287bbf99023c6deff50a3de8f99b
Priyen312 <77093278+Priyen312@users.noreply.github.com>
2022-07-19 17:44:34 +0530

    Time: 17 ms (85.33%), Space: 13.6 MB (72.78%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,7 +1,45 @@
 class Solution {
 public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
         
+//         unordered_map<int, int> m;
+//         for(auto i: nums)
+//         {
+//             m[i]++;
+//         }
+        
+//         vector<int> res;
+        
+//         priority_queue<pair<int, int>> maxHeap;
+//         for(auto i: m)
+//         {
+//             pair<int, int> entry = {i.second, i.first};
+            
+            
+//                 maxHeap.push(entry);
+            
+            
+//             if(maxHeap.size() > m.size() - k)
+//             {
+//                 res.push_back(maxHeap.top().second);
+//                 maxHeap.pop();
+                
+//             }
+//         }
+        
+        
+//         return res;
+        
+        
+//     }
+    struct cmp
+	{
+		bool operator()(pair<int, int> n1, pair<int, int> n2)
+		{
+			return n1.first > n2.first;
+		}
+	};
+    vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int, int> m;
         for(auto i: nums)
         {
@@ -10,26 +48,27 @@ public:
         
         vector<int> res;
         
-        priority_queue<pair<int, int>> maxHeap;
-        for(auto i: m)
+        priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> minHeap;
+        
+        for(auto [value, freq]: m)
         {
-            pair<int, int> entry = {i.second, i.first};
-            
-            
-                maxHeap.push(entry);
-            
-            
-            if(maxHeap.size() > m.size() - k)
+            pair<int, int> entry = {freq, value};
+            if(minHeap.size() < k)
+                minHeap.push(entry);
+            else if(minHeap.top().first < freq)
             {
-                res.push_back(maxHeap.top().second);
-                maxHeap.pop();
-                
+                minHeap.pop();
+                minHeap.push(entry);
             }
         }
         
         
+        while(minHeap.empty() == false)
+        {
+            res.push_back(minHeap.top().second);
+            minHeap.pop();
+        }
+        
         return res;
-        
-        
     }
 };
