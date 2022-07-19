d088a60e8ee63f90019d784edcf360747dfb6374
Priyen312 <77093278+Priyen312@users.noreply.github.com>
2022-07-19 21:38:17 +0530

    Time: 9 ms (34.81%), Space: 13.8 MB (48.50%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
-    void generate(int m, string s,int open, int close, vector<string> &res)
-{
-	if(s.size() == m*2)
-	{
-		// if(!valid(s))
-		// 	return;
-		res.push_back(s);
-		return;
-	}
-	
-	if(open < m) generate(m,s+'(',open + 1, close, res);
-	if(close < open) generate(m,s + ')',open, close+ 1, res);
-	
-}
+    void generate(int m, string &s,int open, int close, vector<string> &res)
+    {//nth catalan number O(4^n/n root(n)), O(4^n/n root(n))
+        if(s.size() == m*2)
+        {
+            // if(!valid(s))
+            // 	return;
+            res.push_back(s);
+            return;
+        }
+
+        if(open < m) 
+        {
+            s = s+'(';
+            generate(m,s,open + 1, close, res);
+            s.pop_back();
+        }
+        if(close < open) 
+        {
+            s = s + ')';
+            generate(m,s,open, close+ 1, res);
+            s.pop_back();
+        }
+
+    }
 vector<string> generateParenthesis(int n) {
     // add your logic here
 	vector<string> res;
 	if(n == 0)
 		return res;
-	generate(n,"",0,0,res);
+    string s = "";
+	generate(n,s,0,0,res);
 	
 	return res;
-}
+    
+    }
 };
