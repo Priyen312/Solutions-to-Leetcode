e79fabd5edcf1a5bbd4bd92668df9d2a7795a0f3
Priyen312 <77093278+Priyen312@users.noreply.github.com>
2022-07-20 11:18:10 +0530

    Time: 5 ms (19.74%), Space: 6.9 MB (10.10%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-     void helper(int i,string curr, string digits,vector<string> mappings, vector<string> &res)
-       { 
+     void helper(int i,string &curr, string digits,vector<string> mappings, vector<string> &res)
+       { //O(4^n) https://www.geeksforgeeks.org/iterative-letter-combinations-of-a-phone-number/
          if(curr.size() == digits.size())
         {
             res.push_back(curr);
@@ -10,8 +10,9 @@ public:
         
         for(auto c : mappings[digits[i] - '0'])
         {
-            
-            helper(i + 1, curr + c ,digits,mappings, res);
+            curr = curr + c; 
+            helper(i + 1, curr ,digits,mappings, res);
+            curr.pop_back();
         }
         
         
@@ -21,7 +22,7 @@ public:
         if(digits.size() == 0) return {};
         
         vector<string> mappings = {"0","1","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        string curr;
+        string curr = "";
         
         helper(0,curr,digits,mappings,res);
   
