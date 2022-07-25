d87c2c2399282870e00c6911c801ef6a724ffd79
Priyen312 <77093278+Priyen312@users.noreply.github.com>
2022-07-25 12:23:50 +0530

    Time: 117 ms (44.59%), Space: 73.4 MB (25.30%) - LeetHub

---
+++ 648-replace-words/648-replace-words.cpp
@@ -0,0 +1,65 @@
+class Trie {
+public:
+    Trie* child[26] = {nullptr};
+    string Pre = "";
+    bool isWord = false;
+};
+
+class Solution {
+public:
+    
+    Trie *root = new Trie();
+    
+    void addWord(string word)
+    {
+        Trie *temp = root;
+        
+        for(char c: word) {
+            if(temp->child[c - 'a'] == nullptr)
+                temp->child[c - 'a'] = new Trie();
+            
+            temp = temp->child[c - 'a'];
+        }
+        
+        temp->Pre = word;
+        temp->isWord = true;
+    }
+    
+    string changePre(string &word)
+    {
+        Trie *temp = root;
+        string prefix = "";
+        for(char c: word) {
+           
+            if(temp->isWord) {
+                return temp->Pre;
+            }
+            
+            if(temp->child[c - 'a'] == nullptr)
+                return word;
+            else
+                temp = temp->child[c - 'a'];
+            
+        }
+                
+        prefix = temp->Pre != "" ? temp->Pre : word;
+        return prefix;
+    }
+    string replaceWords(vector<string>& dictionary, string sentence) {
+        for(auto word: dictionary) {
+            addWord(word);
+        }
+        
+        istringstream s(sentence);
+        string word, ans = "";
+        
+        while(s>>word) {
+            ans += changePre(word) + " ";
+        }
+        
+        if(ans != "")
+            ans.pop_back();
+        
+        return ans;
+    }
+};
