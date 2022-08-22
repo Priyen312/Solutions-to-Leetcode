94a6475eaa9c4adb19eb1d610882d98b6fe77327
Priyen312 <77093278+Priyen312@users.noreply.github.com>
2022-08-22 22:44:19 +0530

    Time: 11 ms (15.72%), Space: 10.9 MB (5.36%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,33 +1,82 @@
 class Solution {
 public:
-    void find_permutations(vector<int>& nums, vector<vector<int>> &permutations, int index)
-    {
-        if(index == nums.size())
-        {
-            permutations.push_back(nums);
+//     void find_permutations(vector<int>& nums, vector<vector<int>> &permutations, int index)
+//     {//https://www.geeksforgeeks.org/write-a-c-program-to-print-all-permutations-of-a-given-string/
+//         // O(n*n!)
+//         if(index == nums.size())
+//         {
+//             permutations.push_back(nums);
+//             return;
+//         }
+        
+//         for(int i = index; i < nums.size(); i++)
+//         {
+//             swap(nums[index], nums[i]);
+            
+//             find_permutations(nums, permutations, index + 1);
+            
+//             swap(nums[index], nums[i]);
+//         }
+        
+        
+        
+//     }
+    
+//     vector<vector<int>> permute(vector<int>& nums) {
+        
+//         vector<vector<int>> permutations;
+        
+        
+//         find_permutations(nums, permutations, 0);
+        
+//         return permutations;
+//     }
+    
+    void backtrack(vector<int> comb, int N, unordered_map<int, int> counter, vector<vector<int>> &results) {
+
+        if (comb.size() == N) {
+            results.push_back(comb);
             return;
         }
-        
-        for(int i = index; i < nums.size(); i++)
+
+        for(auto [num, count]: counter)
         {
-            swap(nums[index], nums[i]);
             
-            find_permutations(nums, permutations, index + 1);
             
-            swap(nums[index], nums[i]);
+            if(count == 0)
+                continue;
+            
+            comb.push_back(num);
+            counter[num] = count - 1;
+            
+            backtrack(comb, N, counter, results);
+            
+            comb.pop_back();
+            counter[num] = count;
+                
         }
         
-        
-        
+            
     }
     
-    vector<vector<int>> permute(vector<int>& nums) {
+     vector<vector<int>> permute(vector <int> nums) {
+        vector<vector<int>> results;
+
         
-        vector<vector<int>> permutations;
+        unordered_map<int, int> counter;
         
-        
-        find_permutations(nums, permutations, 0);
-        
-        return permutations;
+         for (int num : nums) {
+            counter[num]++;
+        }
+
+        vector<int> comb;
+        backtrack(comb, nums.size(), counter, results);
+        return results;
     }
+    
+    
+//                                                       ABC
+//         ABC [swap A, A]                          BAC [swap A, B]                      CBA [swap A, C]
+//             A is fixed                             B is fixed                           C is fixed
+// ABC [swap B, B]   ACB [swap B, C]       BAC [swap A, A]   BCA [swap A, C]      CBA [swap B, B]    CAB [swap B, A]
 };
